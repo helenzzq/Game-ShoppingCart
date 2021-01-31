ef83b904a53d958c2f62b455e9a0a255bfceb86d
helenzzq <helenzhu0411@gmail.com>
2021-01-31 02:16:37 -0500

    Adjust button color and add hover feature

--- checkout-cart/src/App.js
+++ checkout-cart/src/App.js
@@ -5,52 +5,36 @@ import GameList from './GameList'
 import Game from './Component/Game';
 import Summary from './Component/Summary'
 
-function App () {
-
+function App() {
   const { products: games } = GameList;
-  const [item, setCartItems] = useState([]);
-  const deleteItem = (product) => {
-    const exist = item.find((x) => x.id === product.id);
-    if (exist.qty === 1) {
-      removeAll(product)
-    } else {
-      setCartItems(
-        item.map((x) =>
-          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
-        )
-      );
-    }
-  };
-  const removeAll = (product) => {
-    setCartItems(item.filter((x) => x.id !== product.id));
-  }
+  const [item, updateCart] = useState([]);
 
-  const addItem = (product) => {
-    const exist = item.find(x => x.id === product.id);
-    if (exist) {
-      setCartItems(item.map(x => x.id === product.id ? {
-        ...exist, qty: exist.qty + 1
+  const addItem = (gameItem) => {
+    const itemInCart = item.find(k => k.id === gameItem.id);
+    if (itemInCart) {
+      updateCart(item.map(x => x.id === gameItem.id ? {
+        ...itemInCart, num: itemInCart.num + 1
       } : x));
     }
     else {
-      setCartItems([...item,{...product,qty:1}])
+      updateCart([...item, { ...gameItem, num: 1 }])
     }
   }
 
 
-  return ( 
+  return (
     <div className="App">
-      <div className="row">
-      <div className="block col-2">
-      <h1 className="title">Games</h1>
-      <div className="row">
-        {games.map((gameItem) => (
-          <Game key={gameItem.id} product={gameItem} addItem={addItem}></Game>
-        ))}
+      <div className="item">
+        <div className="block col-2">
+          <h1 className="title">Games</h1>
+          <div className="item">
+            {games.map((gameItem) => (
+              <Game key={gameItem.id} gameItem={gameItem} addItem={addItem}></Game>
+            ))}
+          </div>
+        </div>
+        <Summary updateCart={updateCart} addItem={addItem} item={item}></Summary>
       </div>
-        </div>
-        <Summary deleteItem={deleteItem} removeAll={removeAll} addItem={addItem} item = {item}></Summary>
-        </div>
     </div>
   );
 }
