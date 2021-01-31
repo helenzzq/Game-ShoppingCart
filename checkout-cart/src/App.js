f03a324d0d93462d364cbd2b15814601ea838dd3
helenzzq <helenzhu0411@gmail.com>
2021-01-31 09:31:50 -0500

    separte fetchClass

--- checkout-cart/src/App.js
+++ checkout-cart/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
 import GameList from './GameList'
@@ -8,30 +8,35 @@ import Banner from './Component/Banner'
 import background from './photo/background.jpg'
 import addItem from './Component/Summary'
 
+// class fetch extends Component{
+//     componentDidMount(){
+//     fetch(API + DEFAULT_QUERY)
+//       .then(response => response.json())
+//       .then();
+//   }
+//   componentDidUpdate(props) {
+//     //Pass in the quantity of this item in the cart and the item id
+//       const {id , num} = props;
+//     // submit update to database
+//       updateDataBase();
+//   }
+//     updateDataBase(props) {
+
+//   }
+
+//   function getDataBase() {
+
+//     // Array of items
+//     return [];
+//   }
+
+// }
+
+
 function App() {
+  // Current available Game items
   const { games } = GameList;
   const [item, updateCart] = useState([]);
-
-  // function componentDidMount(){
-  //   fetch(API + DEFAULT_QUERY)
-  //     .then(response => response.json())
-  //     .then();
-  // }
-// componentDidUpdate(props) {
-//   const 
-//   // submit update to database
-    // updateDataBase();
-// }
-  function updateDataBase(props) {
-  //Pass in the quantity of this item in the cart and the item id
-    const {id , num} = props;
-}
-
-// function getDataBase() {
-
-//   // Array of items
-//   return [];
-// }
 
 
   return (
