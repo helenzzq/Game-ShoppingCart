7ff35ad741ab789717fd5518bb411dfed8e800b3
zhongjef <jefferson.zhong@mail.utoronto.ca>
2021-02-05 03:32:24 -0800

    Added lambda code

---
+++ checkout-cart/src/lambda/items.js
@@ -0,0 +1,49 @@
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+
+exports.handler = async (event, context) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+
+    let body;
+    let statusCode = '200';
+    const headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    };
+
+    try {
+        switch (event.httpMethod) {
+            // case 'DELETE':
+            //     body = await dynamo.delete(JSON.parse(event.body)).promise();
+            //     break;
+            case 'GET':
+                const itemsTable = process.env.itemsTable;
+                // const data = await dynamo.scan({ TableName: event.queryStringParameters.TableName }).promise();
+                const data = await dynamo.scan({ TableName: itemsTable }).promise();
+                body = data.Items;
+                break;
+            // case 'POST':
+            //     body = await dynamo.put(JSON.parse(event.body)).promise();
+            //     break;
+            case 'PUT':
+                const cart = event.body;
+                // body = await dynamo.update(JSON.parse(event.body)).promise();
+                body = cart;
+                break;
+            default:
+                throw new Error(`Unsupported method "${event.httpMethod}"`);
+        }
+    } catch (err) {
+        statusCode = '400';
+        body = err.message;
+    } finally {
+        body = JSON.stringify(body);
+    }
+
+    return {
+        statusCode,
+        body,
+        headers,
+    };
+};
