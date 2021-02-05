96e885251b88428c54d33862467706e2b30e6f5a
helenzzq <helenzhu0411@gmail.com>
2021-02-05 18:22:43 -0500

    Add icon and adjust css

--- checkout-cart/src/Component/Banner.js
+++ checkout-cart/src/Component/Banner.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import banner from '../photo/banner.jpg';
 import join from '../photo/join.jpg';
+import { HomeOutlined ,PhoneOutlined } from '@ant-design/icons';
+
 export default function Banner(props) {
 
+    function refresh() {
+        window.location.reload();
+    }
+    function contact() {
+        alert("Should you have any question.\n Please contact us through this email : lovecsc301@utoronto.ca")
+    }
     return (
         <header className="banner">
             <div className="bannerImg">
+
                 <img src={banner} alt=""></img>
             </div>
             <nav >
                 <ul className="navBar">
-                    <li>Home</li>
-                    <li>Contact Us</li>
+                    <li onClick={ refresh}><button className="navg"><HomeOutlined className="ant" />Home</button></li>
+                    <li onClick={contact}><button className="navg"><PhoneOutlined className="ant"/> Contact Us</button></li>
                 </ul>
             </nav>
 
