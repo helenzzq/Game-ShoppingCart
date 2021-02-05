94b16093188cf1ad76b9eb7fbafa8185a4ba7354
zhongjef <jefferson.zhong@mail.utoronto.ca>
2021-02-04 23:21:09 -0800

    Added testing code

--- checkout-cart/src/App.js
+++ checkout-cart/src/App.js
@@ -6,7 +6,114 @@ import Game from './Component/Game';
 import Summary from './Component/Summary'
 import Banner from './Component/Banner'
 import background from './photo/background.jpg'
-import {getCartFromDB, updateCartToDB, getAllItemsFromDB} from './Serverless';
+import { nanoid } from 'nanoid';
+
+var LOCAL_STORAGE_KEY_FOR_CARTID = 'csc301-a1-cartId';
+var CARTS_URL = 'https://szae6kjook.execute-api.ca-central-1.amazonaws.com/default/carts';
+var ITEMS_URL = 'https://cbx468lra3.execute-api.ca-central-1.amazonaws.com/default/items';
+
+/**
+ * Return cart from db, an array of items and count,
+ * model:
+ * [
+ *  {
+ *   itemId: string,
+ *   count: Number
+ *  }
+ * ]
+ */
+function getCartFromDB() {
+    // Get current cart id
+    let cartId;
+    cartId = localStorage.getItem(LOCAL_STORAGE_KEY_FOR_CARTID);
+    if (!cartId) {
+      cartId = generateID();
+      localStorage.setItem(LOCAL_STORAGE_KEY_FOR_CARTID, cartId)
+    }
+    console.log("cartId", cartId);
+
+    const url = CARTS_URL + '?cartId=' + cartId;
+    fetch(url, {
+      mode: "cors",
+      method: 'GET',
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(cart => {
+      // TODO: Put data into games
+      console.log("cart", cart);
+    })
+    .catch(err => {
+      console.log("getCartFromDB", err);
+    });
+    
+}
+
+/**
+ * Example
+    items = [{count:2, itemId: "_PI0GfYp"},{count: 3, itemId:"5U76ImCT"}]
+ */
+function updateCartToDB(items) {
+      // Get current cart id
+      let cartId;
+      cartId = localStorage.getItem(LOCAL_STORAGE_KEY_FOR_CARTID);
+      if (!cartId) {
+        cartId = generateID();
+        localStorage.setItem(LOCAL_STORAGE_KEY_FOR_CARTID, cartId)
+      }
+      const url = CARTS_URL;
+      fetch(url, {
+        mode: "cors",
+        method: 'PUT',
+        body: JSON.stringify({
+          cartId: cartId,
+          items: items
+        })
+      })
+      .then(res => {
+        console.log("updateCartToDB", res.status);
+      })
+      .catch(err => {
+        console.log("updateCartToDB", err);
+      });
+}
+
+/**
+ * Return array of items from database,
+ * follows the following model:
+ * [
+ *  {
+ *   id: string,
+ *   image: string,
+ *   name: string,
+ *   price: Number
+ *  }
+ * ]
+ */
+function getAllItemsFromDB() {
+    const url = ITEMS_URL;
+    fetch(url, {
+      mode: "cors",
+      method: 'GET',
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(items => {
+      // TODO: Put data into games
+      console.log("items", items);
+    })
+    .catch(err => {
+      console.log("getAllItemErr", err);
+    });
+}
+
+function generateID() {
+    return nanoid(8);
+}
+  
+
 
 function App() {
   const testItems = [{count:4, itemId: "_PI0GfYp"},{count: 4, itemId:"5U76ImCT"}];
